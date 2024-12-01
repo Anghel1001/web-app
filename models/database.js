05a4488c9203ffa4815a8c6d013cc4cdb389e464
Rhyzelle Salvador <angel.salvador0421@gmail.com>
2024-12-01 22:30:33 +0800

    Update database.js

--- models/database.js
+++ models/database.js
@@ -1,8 +1,13 @@
+const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
-const db = new (require("better-sqlite3"))(path.resolve(__dirname, "../database.db"), {
-  verbose: console.log,  
-});
 
-console.log("Connected to the SQLite database.");
+const dbPath = path.resolve(__dirname, "../database.db");
+const db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+        console.error("Failed to connect to the database:", err.message);
+    } else {
+        console.log("Connected to the SQLite database.");
+    }
+});
 
 module.exports = db;
