d5c1a4881185e2e34187e0ad97fa8ffb21adec4a
Rhyzelle Salvador <angel.salvador0421@gmail.com>
2024-12-01 22:26:00 +0800

    better sql

--- models/database.js
+++ models/database.js
@@ -1,13 +1,8 @@
-const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
-
-const dbPath = path.resolve(__dirname, "../database.db");
-const db = new sqlite3.Database(dbPath, (err) => {
-    if (err) {
-        console.error("Failed to connect to the database:", err.message);
-    } else {
-        console.log("Connected to the SQLite database.");
-    }
+const db = new (require("better-sqlite3"))(path.resolve(__dirname, "../database.db"), {
+  verbose: console.log,  
 });
+
+console.log("Connected to the SQLite database.");
 
 module.exports = db;
