5bbe15d082d3a04a05d0abb9b03bdff61f1c5ff4
Rhyzelle Salvador <angel.salvador0421@gmail.com>
2024-12-02 08:36:20 +0800

    again

--- models/database.js
+++ models/database.js
@@ -1,4 +1,4 @@
-const Database = require('better-sqlite3');
-const db = new Database('./database.db', { verbose: console.log });
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./database.db');
 
 module.exports = db;
